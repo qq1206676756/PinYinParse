46e34c057c6ef7ee523a91361d0eb9cce6fbcd52
qq1206676756 <qq1206676756@qq.com>
2016-12-19 11:35:18 +0800

    修改demo

--- PinYinParse/PinYinParse/Program.cs
+++ PinYinParse/PinYinParse/Program.cs
@@ -29,10 +29,9 @@ namespace PinYinParse
             {
                 Console.WriteLine("请输入中文：");
                 string str = Console.ReadLine();
-                var strs = PinYinConverterHelp.GetTotalPingYin(str).TotalPingYin;
-                var frists = PinYinConverterHelp.GetTotalPingYin(str).FirstPingYin;
-                Console.WriteLine("全拼音：" + String.Join(",", strs));
-                Console.WriteLine("首音：" + String.Join(",", frists));
+                var pingyins = PinYinConverterHelp.GetTotalPingYin(str);
+                Console.WriteLine("全拼音：" + String.Join(",", pingyins.TotalPingYin));
+                Console.WriteLine("首音：" + String.Join(",", pingyins.FirstPingYin));
                 Console.WriteLine();
             }
         }
